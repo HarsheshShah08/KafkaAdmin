4b9a396db82c9d3644f70bd3b549944e44fce201
HarsheshShah08 <harshesh.shah@icloud.com>
2020-06-23 11:25:34 +0100

    first commit

--- src/main/java/com/example/demo/KafkaCreateTopicV3.java
+++ src/main/java/com/example/demo/KafkaCreateTopicV3.java
@@ -6,15 +6,16 @@ import org.apache.kafka.clients.admin.NewTopic;
 import java.util.List;
 import java.util.Map;
 
-public class KafkaCreateTopicV2 {
+public class KafkaCreateTopicV3 {
 
 	public static void main(String[] args) {
 
 		Admin admin = Admin.create(
-				Map.of("bootstrap.servers", "localhost:9092")
+				Map.of("bootstrap.servers", "localhost:9091,localhost:9092,localhost:9093")
 		);
 		try {
-			NewTopic newTopic = new NewTopic("MySecondTopicForDemo", 2, (short) 1);
+			Map<String, String> topicConfig = Map.of("min.insync.replicas", "2");
+			NewTopic newTopic = new NewTopic("MyFirstTopicForDemo", 2, (short) 3).configs(topicConfig);
 			admin.createTopics(List.of(newTopic))
 				.all()
 				.get();
